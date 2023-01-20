d2031ec4c0de6c59d3b5ea3d0da5fc69cda8fbd6
stivex001 <stephenadeyemo@gmail.com>
2023-01-20 23:03:03 +0100

    added mobile menu logic

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,15 @@
-import { Menu } from "@mui/icons-material";
+import { Close, Menu } from "@mui/icons-material";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import Badge from "@mui/material/Badge";
 import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
-import styles from './Header.module.scss'
+import styles from "./Header.module.scss";
 
 const logo = (
   <div className={styles.logo}>
     <Link to="/">
       <h2>
-        Simee.<span>Treat</span> 
+        Simee.<span>Treat</span>
       </h2>
     </Link>
   </div>
@@ -17,54 +17,75 @@ const logo = (
 
 const cart = (
   <span className={styles.cart}>
-              <NavLink to="/cart">
-                <Badge badgeContent={4} color="primary">
-                  <ShoppingCartOutlinedIcon />
-                </Badge>
-              </NavLink>
-            </span>
-)
+    <NavLink to="/cart">
+      <Badge badgeContent={4} color="primary">
+        <ShoppingCartOutlinedIcon style={{ fontSize: "40px" }} />
+      </Badge>
+    </NavLink>
+  </span>
+);
+
+const activeLink = ({ isActive }) => (isActive ? `${styles.active}` : "");
 
 const Header = () => {
-  const [showMenu, setShowMenu] = useState(false)
+  const [showMenu, setShowMenu] = useState(false);
 
   const toggleMenu = () => {
-    setShowMenu(showMenu => !showMenu)
-  }
+    setShowMenu((showMenu) => !showMenu);
+  };
 
-  // const hideMenu = () => {
-  //   setShowMenu(prev => !prev)
-  // }
+  const hideMenu = () => {
+    setShowMenu(false);
+  };
 
   return (
     <header>
       <div className={styles.header}>
         {logo}
 
-        <nav className={
-              showMenu ? `${styles["show-nav"]}` : `${styles["hide-nav"]}`
-            }>
-          <div className={
-                showMenu
-                  ? `${styles["nav-wrapper"]} ${styles["show-nav-wrapper"]}`
-                  : `${styles["nav-wrapper"]}`
-              }>
-              </div>
-          <ul>
-            <li>
-              <Link to="/admin">Admin</Link>
+        <nav
+          className={
+            showMenu ? `${styles["show-nav"]}` : `${styles["hide-nav"]}`
+          }
+        >
+          <div
+            className={
+              showMenu
+                ? `${styles["nav-wrapper"]} ${styles["show-nav-wrapper"]}`
+                : `${styles["nav-wrapper"]}`
+            }
+            onClick={hideMenu}
+          ></div>
+
+          <ul onClick={hideMenu}>
+            <li className={styles["logo-mobile"]}>
+              {logo}
+              <Close style={{ fontSize: "30px" }} onClick={hideMenu} />
             </li>
             <li>
-              <Link to="/about">About Us</Link>
+              <NavLink className={activeLink} to="/admin">
+                Admin
+              </NavLink>
             </li>
             <li>
-              <Link to="/contact">Contact Us</Link>
+              <NavLink className={activeLink} to="/about">
+                About Us
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className={activeLink} to="/contact">
+                Contact Us
+              </NavLink>
             </li>
           </ul>
-          <div className={styles["header-right"]}>
+          <div className={styles["header-right"]} onClick={hideMenu}>
             <span className={styles.links}>
-              <NavLink to="/login">Login</NavLink>
-              <NavLink to="/register">Register</NavLink>
+              <NavLink className={activeLink} to="/login">
+                Login
+              </NavLink>
+              <NavLink className={activeLink} to="/register">
+                Register
+              </NavLink>
             </span>
             {cart}
           </div>
@@ -72,7 +93,7 @@ const Header = () => {
 
         <div className={styles["menu-icon"]}>
           {cart}
-          <Menu  style={{fontSize: '50px'}} onClick={toggleMenu} />
+          <Menu style={{ fontSize: "50px" }} onClick={toggleMenu} />
         </div>
       </div>
     </header>
--- src/components/slider/slider-data.js
+++ src/components/slider/slider-data.js
@@ -8,15 +8,22 @@ export const sliderData = [
   },
   {
     image:
-      "http://crems.like-themes.com/wp-content/uploads/2019/06/shop-07-480x480.png",
+      "https://restroking.dexignzone.com/react/static/media/pic1.81227e0f.jpg",
     desc: "Satisfying Your Taste Bud",
     heading: "Up to 30% off on all onsale proucts.",
     
   },
   {
     image:
-      "http://crems.like-themes.com/wp-content/uploads/2019/06/shop-01-480x480.png",
+      "https://images.unsplash.com/photo-1578985545062-69928b1d9587?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1089&q=80",
       desc: "Satisfying Your Taste Bud",
       heading: "Up to 30% off on all onsale proucts.",
+  },
+  {
+    image:
+      "https://media.istockphoto.com/id/1130350948/photo/five-fresh-donuts-with-various-fillings-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=qlxSWYdHqp30W6muD41p8GrMpMIUBERB9aC6r_AIz2g=",
+    desc: "Satisfying Your Taste Bud",
+    heading: "Up to 30% off on all onsale proucts.",
+    
   },
 ];
