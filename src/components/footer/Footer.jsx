ecd5f50fb02b162fd122504a16c377769db5f104
stivex001 <stephenadeyemo@gmail.com>
2023-01-21 02:40:09 +0100

    feat: implemented footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,139 @@
-import React from 'react'
+import {
+  Facebook,
+  Instagram,
+  MailOutline,
+  Phone,
+  Room,
+  Twitter,
+  WhatsApp,
+} from "@mui/icons-material";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+`;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+const Logo = styled.h1``;
+
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+
+const SocialContainer = styled.div`
+  display: flex;
+  cursor: pointer;
+`;
+
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 20px;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const ContactItem = styled.div`
+margin-bottom: 20px;
+display: flex;
+align-items: center;
+`;
+
+const Payment = styled.img`
+width: 50%;
+`;
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <Container>
+      <Left>
+        <Logo>Simee.Treat</Logo>
+        <Desc>
+          Simee.treats is a brand that provides freshly-baked products such as
+          cakes, cupcakes, meat pie, doughnuts, chinchin and lots more for all
+          events and personal consumption at affordable prices. Also, yout
+          satisfaction is guaranteed
+        </Desc>
+        <SocialContainer>
+          <SocialIcon color="3b5999">
+            <Facebook />
+          </SocialIcon>
+          <SocialIcon color="e4405f">
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon color="55acee">
+            <Twitter />
+          </SocialIcon>
+          <SocialIcon color="128C7E">
+            <WhatsApp />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
 
-export default Footer
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Cakes</ListItem>
+          <ListItem>Cup Cakes</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>WishList</ListItem>
+          <ListItem>Terms </ListItem>
+        </List>
+      </Center>
+
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <Room style={{marginRight: '10px'}} /> Shagari Village, 340110, Akure, Nigeria
+        </ContactItem>
+        <ContactItem>
+          <Phone style={{marginRight: '10px'}} /> +234 7061668679
+        </ContactItem>
+        <ContactItem>
+          <MailOutline style={{marginRight: '10px'}} /> simee.treats@gmail.com
+        </ContactItem>
+        <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+      </Right>
+    </Container>
+  );
+};
+
+export default Footer;
