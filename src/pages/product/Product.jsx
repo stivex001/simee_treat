3939201948cbf34cced0675c1478de4c8d43bab1
stivex001 <stephenadeyemo@gmail.com>
2023-01-29 21:47:37 +0100

    feat:added redux package

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,5 +1,4 @@
 import { Add, Remove } from "@mui/icons-material";
-import meatpie from "../../assets/meatpie.jpg";
 import { useLocation } from "react-router-dom";
 import {
   AddContainer,
@@ -27,57 +26,63 @@ import { publicRequest } from "../../requestMethods";
 const Product = () => {
   const location = useLocation();
   const id = location.pathname.split("/")[2];
-  const [product, setProduct] = useState({})
+  const [product, setProduct] = useState({});
+  const [quantity, setQuantity] = useState(1);
+  const [size, setSize] = useState("");
 
   useEffect(() => {
     const getProduct = async () => {
       try {
-        const res = await publicRequest.get(`/product/${id}`)
-        setProduct(res.data)
-      } catch (error) {
-        
-      }
-    }
-    getProduct()
-  },[id])
+        const res = await publicRequest.get(`/product/${id}`);
+        setProduct(res.data);
+      } catch (error) {}
+    };
+    getProduct();
+  }, [id]);
+
+  const addQuantityHandler = () => {
+    setQuantity((prev) => prev + 1);
+  };
+
+  const removeQuantityHandler = () => {
+    quantity > 1 && setQuantity((prev) => prev - 1);
+  };
+
+  const handleClick = () => {};
 
   return (
     <Container>
       <Wrapper>
         <ImageContainer>
-          <Image src={product.img} alt="jsdkhudsh" />
+          <Image src={product.img} alt={product.title} />
         </ImageContainer>
         <InfoContainer>
           <Title>{product.title}</Title>
-          <Desc>
-            {product.desc}
-          </Desc>
+          <Desc>{product.desc}</Desc>
           <Price>#{product.price}</Price>
           <FilterContainer>
-            <Filter>
+            {/* <Filter>
               <FilterTitle>Color</FilterTitle>
-              <FilterColor color="black" />
-              <FilterColor color="darkblue" />
-              <FilterColor color="gray" />
-            </Filter>
+              {product.colour?.map((c) => (
+                <FilterColor color={c} key={c} />
+              ))}
+            </Filter> */}
             <Filter>
               <FilterTitle>Size</FilterTitle>
-              <FilterSize>
-                <FilterSizeOption>B</FilterSizeOption>
-                <FilterSizeOption>XL</FilterSizeOption>
-                <FilterSizeOption>M</FilterSizeOption>
-                <FilterSizeOption>L</FilterSizeOption>
-                <FilterSizeOption>S</FilterSizeOption>
+              <FilterSize onChange={(e) => setSize(e.target.value)}>
+                {product.size?.map((s) => (
+                  <FilterSizeOption key={s}>{s}</FilterSizeOption>
+                ))}
               </FilterSize>
             </Filter>
           </FilterContainer>
           <AddContainer>
             <AmountContainer>
-              <Remove />
-              <Amount>1</Amount>
-              <Add />
+              <Remove onClick={removeQuantityHandler} />
+              <Amount>{quantity}</Amount>
+              <Add onClick={addQuantityHandler} />
             </AmountContainer>
-            <Button>ADD TO CART</Button>
+            <Button onClick={handleClick}>ADD TO CART</Button>
           </AddContainer>
         </InfoContainer>
       </Wrapper>
