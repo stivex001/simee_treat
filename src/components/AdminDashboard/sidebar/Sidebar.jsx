ca423e951ff822dcf950d057a43351b6c1b0506f
stivex001 <stephenadeyemo@gmail.com>
2023-01-30 17:05:33 +0100

    feat: implemented Sidebar components

--- src/components/AdminDashboard/sidebar/Sidebar.jsx
+++ src/components/AdminDashboard/sidebar/Sidebar.jsx
@@ -1,9 +1,157 @@
-import React from 'react'
+import { AttachMoney, ChatBubbleOutline, DynamicFeed, LineStyle, MailOutline, PermIdentity, Report, Storefront, Timeline, TrendingUp, WorkOutline } from "@mui/icons-material";
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  flex: 1;
+  height: calc(100vh - 50px);
+  background-color: rgb(251, 251, 255);
+  position: sticky;
+  top: 50px;
+`;
+
+const Wrapper = styled.div`
+  padding: 20px;
+  color: #555;
+`;
+
+const Menu = styled.div`
+  margin-bottom: 10px;
+`;
+
+const Title = styled.h3`
+  font-size: 13px;
+  color: rgb(187, 186, 186);
+`;
+const List = styled.ul`
+  list-style: none;
+  padding: 5px;
+`;
+const ListItem = styled.li`
+  padding: 5px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  border-radius: 10px;
+
+  &:active {
+    background-color: rgb(240, 240, 255);
+  }
+
+  &:hover {
+    background-color: rgb(240, 240, 255);
+  }
+`;
+const Icon = styled.div`
+margin-right: 5px;
+font-size: 20px;
+`;
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
-}
+    <Container>
+      <Wrapper>
+        <Menu>
+          <Title>Dashboard</Title>
+          <List>
+            <ListItem>
+              <Icon>
+                <LineStyle />
+              </Icon>
+              Home
+            </ListItem>
+            <ListItem>
+              <Icon>
+                <Timeline />
+              </Icon>
+              Analytics
+            </ListItem>
+            <ListItem>
+              <Icon>
+                <TrendingUp />
+              </Icon>
+              Sales
+            </ListItem>
+          </List>
+        </Menu>
+        <Menu>
+          <Title>Quick Menu</Title>
+          <List>
+            <ListItem>
+              <Icon>
+                <PermIdentity />
+              </Icon>
+              Users
+            </ListItem>
+            <ListItem>
+              <Icon>
+                <Storefront />
+              </Icon>
+              Products
+            </ListItem>
+            <ListItem>
+              <Icon>
+                <AttachMoney />
+              </Icon>
+              Transactions
+            </ListItem>
+            <ListItem>
+              <Icon>
+                <TrendingUp />
+              </Icon>
+              Reports
+            </ListItem>
+          </List>
+        </Menu>
+        <Menu>
+          <Title>Notifications</Title>
+          <List>
+            <ListItem>
+              <Icon>
+                <MailOutline />
+              </Icon>
+              Mail
+            </ListItem>
+            <ListItem>
+              <Icon>
+                <DynamicFeed />
+              </Icon>
+              Feedback
+            </ListItem>
+            <ListItem>
+              <Icon>
+                <ChatBubbleOutline />
+              </Icon>
+              Messages
+            </ListItem>
+          </List>
+        </Menu>
+        <Menu>
+          <Title>Staff</Title>
+          <List>
+            <ListItem>
+              <Icon>
+                <WorkOutline />
+              </Icon>
+              Manage
+            </ListItem>
+            <ListItem>
+              <Icon>
+                <Timeline />
+              </Icon>
+              Analytics
+            </ListItem>
+            <ListItem>
+              <Icon>
+                <Report />
+              </Icon>
+              Reports
+            </ListItem>
+          </List>
+        </Menu>
+      </Wrapper>
+    </Container>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
+import styled from 'styled-components';
 import Sidebar from '../../components/AdminDashboard/sidebar/Sidebar'
 import Topbar from '../../components/AdminDashboard/topbar/Topbar'
+
+const Container = styled.div`
+display: flex;
+`;
+const Other = styled.div`
+flex: 4;
+`;
 
 const Admin = () => {
   return (
     <div>
       <Topbar />
-      <Sidebar />
+      <Container>
+         <Sidebar />
+         <Other>others</Other>
+      </Container>
+     
     </div>
   )
 }
