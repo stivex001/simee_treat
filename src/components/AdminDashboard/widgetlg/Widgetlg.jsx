c62dd5811ccdb63d35dc9b438728c49520f8d442
stivex001 <stephenadeyemo@gmail.com>
2023-01-30 20:56:51 +0100

    implemted widget component ui

---
+++ src/components/AdminDashboard/widgetlg/Widgetlg.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+flex: 2;
+box-shadow: 0px 0px 15px -10px rgba(0, 0, 0, 0.75);
+  -webkit-box-shadow: 0px 0px 15px -10px rgba(0, 0, 0, 0.75);
+  -moz-box-shadow: 19px 5px 50px -3px rgba(235, 180, 180, 0.55);
+  padding: 20px;
+`;
+
+
+const Widgetlg = () => {
+  return (
+    <Container>Widgetlg</Container>
+  )
+}
+
+export default Widgetlg
--- src/pages/admin/home/Home.jsx
+++ src/pages/admin/home/Home.jsx
@@ -2,9 +2,16 @@ import styled from "styled-components";
 import Chart from "../../../components/AdminDashboard/chart/Chart";
 import { userData } from "../../../components/AdminDashboard/chart/data";
 import FeaturedInfo from "../../../components/AdminDashboard/featurerdInfo/FeaturedInfo";
+import Widgetlg from "../../../components/AdminDashboard/widgetlg/Widgetlg";
+import Widgetsm from "../../../components/AdminDashboard/widgetsm/Widgetsm";
 
 const Container = styled.div`
 flex: 4;
+`;
+
+const Widget = styled.div`
+display: flex;
+justify-content: space-between;
 `;
 
 const Home = () => {
@@ -12,6 +19,10 @@ const Home = () => {
     <Container>
         <FeaturedInfo />
         <Chart data={userData} title="User Analytics" grid dataKey="Active User" />
+        <Widget>
+            <Widgetsm />
+            <Widgetlg />
+        </Widget>
     </Container>
   )
 }
