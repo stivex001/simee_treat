1df2989bee68243a00ae9fe8ef19b8eba681f184
stivex001 <stephenadeyemo@gmail.com>
2023-01-30 16:25:10 +0100

    feat: created sidebar component

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Sidebar from '../../components/AdminDashboard/sidebar/Sidebar'
 import Topbar from '../../components/AdminDashboard/topbar/Topbar'
 
 const Admin = () => {
   return (
     <div>
       <Topbar />
+      <Sidebar />
     </div>
   )
 }
