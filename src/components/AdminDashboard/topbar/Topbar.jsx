56956def760c4ce2d8fce98d042aa2dbf48a27ff
stivex001 <stephenadeyemo@gmail.com>
2023-01-30 16:22:46 +0100

    feat: added padding amd color to navbar

--- src/components/AdminDashboard/topbar/Topbar.jsx
+++ src/components/AdminDashboard/topbar/Topbar.jsx
@@ -1,63 +1,97 @@
-import { NotificationsNone } from '@mui/icons-material';
-import React from 'react'
-import styled from 'styled-components';
+import { Language, NotificationsNone, Settings } from "@mui/icons-material";
+import React from "react";
+import styled from "styled-components";
 
 const Container = styled.div`
-    width: 100%;
-    height: 50px;
-    background-color: white;
+  width: 100%;
+  height: 50px;
+  background-color: white;
+  position: sticky;
+  top: 0;
+  z-index: 999;
 `;
 
 const Wrapper = styled.div`
-    height: 100%;
-    padding: 0px 20px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+  height: 100%;
+  padding: 0px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
-const Left = styled.div`
-
-`;
+const Left = styled.div``;
 
 const Span = styled.span`
-    font-weight: bold;
-    font-size: 30px;
-    color: orangered;
-    cursor: pointer;
+  font-weight: bold;
+  font-size: 30px;
+  color: orangered;
+  cursor: pointer;
 `;
 
 const Right = styled.div`
-
+  display: flex;
+  align-items: center;
 `;
 
 const IconContainer = styled.div`
-position: relative;
+  position: relative;
+  cursor: pointer;
+  margin-right: 10px;
+  color: #555;
 `;
 
 const IconBadge = styled.span`
-position: absolute;
-top: -5px;
-right: 5px;
-background-color: orangered;
+  width: 15px;
+  height: 15px;
+  position: absolute;
+  top: -5px;
+  right: 5px;
+  background-color: orangered;
+  color: white;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  font-size: 10px;
+`;
+
+const ImageContainer = styled.div``;
+
+const Image = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  cursor: pointer;
 `;
 
 const Topbar = () => {
   return (
     <Container>
-        <Wrapper>
-            <Left>
-                <Span>Simee.TreatAdmin</Span>
-            </Left>
-            <Right>
-                <IconContainer>
-                    <NotificationsNone />
-                    <IconBadge>2</IconBadge>
-                </IconContainer>
-            </Right>
-        </Wrapper>
+      <Wrapper>
+        <Left>
+          <Span>Simee.TreatAdmin</Span>
+        </Left>
+        <Right>
+          <IconContainer>
+            <NotificationsNone />
+            <IconBadge>2</IconBadge>
+          </IconContainer>
+          <IconContainer>
+            <Language />
+            <IconBadge>2</IconBadge>
+          </IconContainer>
+          <IconContainer>
+            <Settings />
+          </IconContainer>
+          <ImageContainer>
+            <Image
+              src="https://avatars.githubusercontent.com/u/87942124?v=4"
+              alt="admin"
+            />
+          </ImageContainer>
+        </Right>
+      </Wrapper>
     </Container>
-  )
-}
+  );
+};
 
-export default Topbar
+export default Topbar;
