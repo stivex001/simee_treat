94db9a4f4022417696fd0408d91d0375cbbda1e2
stivex001 <stephenadeyemo@gmail.com>
2023-01-31 01:20:12 +0100

    fix: added padding to status button

--- src/components/AdminDashboard/widgetlg/Widgetlg.jsx
+++ src/components/AdminDashboard/widgetlg/Widgetlg.jsx
@@ -11,22 +11,44 @@ const WidgetTitle = styled.span`
   font-size: 22px;
   font-weight: 600;
 `;
-const WidgetTable = styled.table``;
-const Tr = styled.tr``;
-const Th = styled.th``;
-const Td = styled.td``;
-const Image = styled.img`
-width: 40px;
-height: 40px;
-border-radius: 50%;
-object-fit: cover;
+const WidgetTable = styled.table`
+width: 100%;
+border-spacing: 20px;
 `;
-const Username = styled.span`
+const Tr = styled.tr``;
+const Th = styled.th`
+text-align: left;
+`;
+const Td = styled.td`
+display: flex;
+align-items: center;
 font-weight: 600;
 `;
-const Date = styled.td``;
-const Amount = styled.td``;
+const Image = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  object-fit: cover;
+  margin-right: 10px;
+`;
+const Username = styled.span`
+  font-weight: 600;
+`;
+const Date = styled.td`
+font-weight: 300;
+`;
+const Amount = styled.td`
+font-weight: 300;
+`;
 const Status = styled.td``;
+const WidgetButton = styled.button`
+  padding: 5px 7px;
+  border: none;
+  border-radius: 10px;
+  background-color: ${prop => prop.color};
+  color: ${prop => prop.textColor};
+`;
+
 
 const Widgetlg = () => {
   return (
@@ -34,19 +56,54 @@ const Widgetlg = () => {
       <WidgetTitle>Latest Transactions</WidgetTitle>
       <WidgetTable>
         <Tr>
-            <Th>Customer</Th>
-            <Th>Date</Th>
-            <Th>Amount</Th>
-            <Th>Status</Th>
+          <Th>Customer</Th>
+          <Th>Date</Th>
+          <Th>Amount</Th>
+          <Th>Status</Th>
         </Tr>
         <Tr>
-            <Td>
+          <Td>
             <Image src="https://avatars.githubusercontent.com/u/87942124?v=4" />
             <Username>Adeyemo Stephen</Username>
-            </Td>
-            <Date>2 Jub 2022</Date>
-            <Amount>#1500</Amount>
-            <Status></Status>
+          </Td>
+          <Date>2 Jub 2022</Date>
+          <Amount>#1500</Amount>
+          <Status>
+            <WidgetButton color="#e5faf2" textColor="#3bb077">Approved</WidgetButton>
+          </Status>
+        </Tr>
+        <Tr>
+          <Td>
+            <Image src="https://avatars.githubusercontent.com/u/87942124?v=4" />
+            <Username>Adeyemo Stephen</Username>
+          </Td>
+          <Date>2 Jub 2022</Date>
+          <Amount>#1500</Amount>
+          <Status>
+            <WidgetButton color="#fff0f1" textColor="#d95087">Declined</WidgetButton>
+          </Status>
+        </Tr>
+        <Tr>
+          <Td>
+            <Image src="https://avatars.githubusercontent.com/u/87942124?v=4" />
+            <Username>Adeyemo Stephen</Username>
+          </Td>
+          <Date>2 Jub 2022</Date>
+          <Amount>#1500</Amount>
+          <Status>
+            <WidgetButton color="#ebf1e" textColor="#2a7ade">Pending</WidgetButton>
+          </Status>
+        </Tr>
+        <Tr>
+          <Td>
+            <Image src="https://avatars.githubusercontent.com/u/87942124?v=4" />
+            <Username>Adeyemo Stephen</Username>
+          </Td>
+          <Date>2 Jub 2022</Date>
+          <Amount>#1500</Amount>
+          <Status>
+            <WidgetButton color="#e5faf2" textColor="#3bb077">Approved</WidgetButton>
+          </Status>
         </Tr>
       </WidgetTable>
     </Container>
